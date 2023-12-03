b011fb066bf77f82607156311b4db9e026feb7ed
Gjorgjieva Forka <149876641+GjorgjievaF@users.noreply.github.com>
2023-12-03 16:54:23 +0100

    Add files via upload

---
+++ HomeWork#10-Variables/script.js
@@ -0,0 +1,58 @@
+// Exerscise 1.
+// 1 feet = 0.3048;
+// 1.I shuld have the value 1 feet to meters ratio;
+// 2.I shuld have the meters;
+// 3.To get the result i should multiply feet valeu with meters ratio;
+
+let oneFeet=0.3048;
+let metersValue=30;
+
+
+let result=oneFeet*metersValue;
+
+console.log(result)
+
+// Exercise 2.
+// TIP: area=a*b
+// 
+// 1.I should have the length of side a;
+// 2.I shoul have the width of side b;
+// 3.To find the area i should multiply sides a and sides b;
+
+
+let sideA=3;
+let sideB=5;
+
+let calculateArea=sideA*sideB;
+
+console.log(calculateArea)
+
+// Exercise 3.
+// TIP: area=3.14(pi)*r2
+// 1.I should know the pi
+// 2.I should know the length of the radius
+// 3.I should find the area by multiplying the pi and radius
+
+let piValue=3.14;
+let radiusLength=2*2;
+
+let areaResult=piValue*radiusLength;
+console.log(areaResult)
+
+// Exercise 4 (Home work)
+// The price of one phone is $119.95
+// The tax rate for everi phone is 5%
+// 1.I shoul know the price of a single phone
+// 2.I should know tax rate 
+// 3.I should add the tax on the phone price
+// 4.I should multiply by 30
+
+let phonePrice=119.95;
+let taxOfPhone=119.95%5;
+
+let finalPhonePrice=phonePrice+taxOfPhone;
+
+let multiplicationPhones=finalPhonePrice*30;
+console.log(multiplicationPhones)
+
+
